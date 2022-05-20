c7e53e1611c40cf26d146403085b370debc4598d
Victor Nestor <victor.n.x35@gmail.com>
2022-05-20 01:40:39 -0400

    Working on Login authorization.

---
+++ spoofy-client/src/components/Homepage/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import Login from "../Login";
+
+export default function Homepage() {
+  return <Login />;
+}
--- spoofy-client/src/components/Login/index.js
+++ spoofy-client/src/components/Login/index.js
@@ -1,4 +1,7 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+// export const StyledContainer = styled(Container)``;
+import { StyledButton } from "./LoginElements";
 
 //authorization ref: https://developer.spotify.com/documentation/general/guides/authorization/code-flow/
 //scope ref: https://developer.spotify.com/documentation/general/guides/authorization/scopes/
@@ -7,5 +10,11 @@ const URL =
   "https://accounts.spotify.com/authorize?client_id&response_type=code&redirect_url=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state";
 
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <Container>
+      <StyledButton type="button" className="btn btn-success btn lg" href={URL}>
+        Login With Spotify
+      </StyledButton>
+    </Container>
+  );
 }
